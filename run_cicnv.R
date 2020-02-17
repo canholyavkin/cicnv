273684bf9baf536f174090c83933865fc4189c82
canholyavkin <canholyavkin@icloud.com>
2020-02-18 02:46:05 +0300

    Add data import&clean section

--- run_cicnv.R
+++ run_cicnv.R
@@ -1,5 +1,8 @@
 # Packages ----------------------------------------------------------------
 library(valr)
 library(dplyr)
+library(stringr)
 
-# Data Import -------------------------------------------------------------
+# Data Import & Cleaning --------------------------------------------------
+rd <- read_bed("./data/raw/sample_interval.bed") %>%
+  mutate(chrom = ifelse(str_detect(chrom,"chr"), chrom, paste0("chr",chrom)))
