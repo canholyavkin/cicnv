b6bc397a60294306e8978c56d9bf26871d8ac70e
canholyavkin <canholyavkin@icloud.com>
2020-02-29 16:14:58 +0300

    Add query parser and update evidence functions

--- src/create_db.R
+++ src/create_db.R
@@ -3,7 +3,7 @@
 
 # Packages ----------------------------------------------------------------
 library(tidyverse)
-
+library(valr)
 
 # RefSeq 105v2 ------------------------------------------------------------
 refseq <- vroom::vroom("./data/reference/refseq_105v2.txt.gz",
@@ -17,7 +17,7 @@ refseq <- vroom::vroom("./data/reference/refseq_105v2.txt.gz",
 protein_coding_regions <- refseq %>% 
   filter(transcript_type=="mRNA") %>% 
   separate_rows(exon_starts, exon_stops, sep = ",") %>% 
-  select(chrom, start = exon_starts, end = exon_stops, name) %>% 
+  select(chrom, start = exon_starts, end = exon_stops, name, gene_name) %>% 
   mutate(start = as.numeric(start) + 1,
          end = as.numeric(end)) %>%
   tbl_interval()
@@ -41,7 +41,6 @@ hi_regions <- read.delim("ftp://ftp.clinicalgenome.org/ClinGen_region_curation_l
          end = as.numeric(end)) %>%
   tbl_interval()
 
-
 # 5' Haploinsufficient Genes ----------------------------------------------
 hi_genes_5p <- refseq %>%
   filter(gene_name %in% hi_genes$name,
@@ -51,5 +50,18 @@ hi_genes_5p <- refseq %>%
   mutate(start = as.character(start),
          end = as.character(end)) %>% 
   create_utrs5() %>% 
-  mutate(start = as.numeric(start),
+  mutate(start = as.numeric(start) + 1,
+         end   = as.numeric(end))
+
+# 3' Haploinsufficient Genes ----------------------------------------------
+hi_genes_3p <- refseq %>%
+  filter(gene_name %in% hi_genes$name,
+         transcript_type == "mRNA") %>% 
+  rename(cds_end = cds_stop) %>% 
+  select(chrom, start, end, name, cds_start, cds_end, strand, gene_name) %>% 
+  mutate(start = as.character(start),
+         end = as.character(end)) %>% 
+  create_utrs3() %>% 
+  left_join(., select(refseq, gene_name, name), by = "name") %>% 
+  mutate(start = as.numeric(start) + 1,
          end   = as.numeric(end))
--- src/evidence_functions.R
+++ src/evidence_functions.R
@@ -1,8 +1,18 @@
+query_parser <- function(x) {
+  custom_target <- x %>% str_remove_all(",")
+  custom_chr    <- str_remove(custom_target, ":.*") %>% paste0("chr",.)
+  custom_start  <- str_remove(custom_target, ".*: ") %>% str_remove(" .*") %>% as.numeric()
+  custom_end    <- str_remove(custom_target, ".*- ") %>% as.numeric()
+  return(tibble::tribble(~chrom, ~start, ~end, custom_chr, custom_start, custom_end))
+}
+
 loss_1 <- function(x) {
-  query   <- trbl_interval(~ chrom, ~ start, ~ end, 
-                           x[[1]], as.numeric(x[[2]]), as.numeric(x[[3]]))
-  ifelse(nrow(bed_intersect(query, protein_coding_regions)) > 0 | 
-           nrow(bed_intersect(query, hi_regions)) > 0 , 0, -0.6)
+  query = query_parser(x)
+  ol_pc_regions = bed_intersect(query, protein_coding_regions)
+  ol_hi_regions = bed_intersect(query, hi_regions)
+  if (nrow(ol_pc_regions) > 0 | nrow(ol_hi_regions) > 0) {
+    print("The region contains protein-coding or other known functionally important elements. (Score: 0.00 - Continue evaluation")
+  } else("The region Does NOT contain protein-coding or any known functionally important elements. (Score: -0.60)")
 }
 
 loss_2a <- function(x) {
@@ -29,11 +39,18 @@ loss_2b <- function(x) {
   if_else(nrow(int_hi_genes) > 0 | nrow(int_hi_regions) > 0, T, F)
 }
 
-loss_2c <- function(x) {
-  query   <- trbl_interval(~ chrom, ~ start, ~ end,
-                           x[[1]], as.numeric(x[[2]]), as.numeric(x[[3]]))
-  int_hi_genes_5p <- bed_intersect(query, hi_genes_5p) %>%
-    mutate(complete_overlap = (.overlap == end.y - start.y))
-  
-  if_else(any(int_hi_genes_5p$complete_overlap), 1, 0.45)
+loss_2c1 <- function(x) {
+  query <- query_parser(x)
+  ol_3p_regions <- bed_intersect(query, hi_genes_3p)
+  if (nrow(ol_3p_regions) == 0) {   # If query has no overlap with 3P of HI
+    ol_5p_regions = bed_intersect(query, hi_genes_5p)
+    if (nrow(ol_5p_regions) == 0) {
+      print("FALSE. The region has no overlap with 5' of a haploinsuficient gene.")
+    }
+  } else {
+    print(paste0("FALSE. ",
+                 "The region has overlap with 3' of a haploinsuficient gene (",
+                 paste(unique(ol_3p_regions$gene_name.y), collapse = ", "),
+                 ")."))
+  }
 }
